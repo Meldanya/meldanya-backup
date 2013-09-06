00e43f3277ea8d8ba9df2fa922a1bd6187d6780b
Erik Jansson <erikjansson90@gmail.com>
2013-09-06 17:08:45 +0200

    Add comments to config class

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,20 @@
-'''The config singleton class.'''
-# System includes
 import sys
 import json
 import logging
 
 
 class Config(dict):
+    """The config class which functions exactly as a dict but reads the content
+    of the dict from the config file.
 
-    def __init__(self, config_file, defaults):
+    If the config file can't be opened or parsed, the class will output the
+    error and then exit the entire program. We can't really go on without
+    configuration (e.g. what should we backup?).
+
+    :param config_file: the config file to read from.
+    :param defaults: a dict of default items (optional)"""
+
+    def __init__(self, config_file, defaults=None):
         dict.__init__(self, defaults or {})
         exit = False
         try:
